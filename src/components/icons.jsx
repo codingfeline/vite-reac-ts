4dc75e6c0b69688db49d3447874173ee29186964
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-21 16:45:00 +0100

    customising FontAwesome

---
+++ src/components/icons.jsx
@@ -0,0 +1,9 @@
+export { FontAwesomeIcon as Awe } from '@fortawesome/react-fontawesome'
+
+export { faEnvelope as envelope } from '@fortawesome/free-solid-svg-icons'
+export { faHouse } from '@fortawesome/free-solid-svg-icons'
+export { faCircle } from '@fortawesome/free-solid-svg-icons'
+
+export { faCircle as faCircle2 } from '@fortawesome/free-regular-svg-icons'
+export { faLemon } from '@fortawesome/free-regular-svg-icons'
+export { faUtensils as utensils } from '@fortawesome/free-solid-svg-icons'
