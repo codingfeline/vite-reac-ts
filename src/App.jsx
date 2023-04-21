c44a202f9e62ea88e0fd1a1abce6c31f577c9154
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-21 12:10:24 +0100

    added sticky footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 import './App.css'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from './components/home'
-import Restaurants from './components/restaurants'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import About from './components/about'
+import AuthBox from './components/auth'
+import Restaurants from './components/restaurants'
+import OneRestaurant from './components/OneRestaurant'
 
 function App() {
   return (
     <Router>
-      <div className="bg-blue-100  flex  flex-col  ">
+      <div className="ns_box bg-purple-300 flex flex-col h-screen justify-between">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/restaurants" element={<Restaurants />} />
-        </Routes>
+        <div className="bg-lime-100  flex  flex-col mb-auto   ">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/auth" element={<AuthBox />} />
+            <Route path="/restaurants" element={<Restaurants />} />
+            <Route path="/restaurant/:id" element={<OneRestaurant />} />
+          </Routes>
+        </div>
         <Footer />
       </div>
     </Router>
