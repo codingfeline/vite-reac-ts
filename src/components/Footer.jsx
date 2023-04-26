4c1fa626c106ecb1633dac2a0577047d30c69c85
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-26 22:06:42 +0100

    added footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="bg-slate-200 p-5 pl-0 border-0 border-t border-blue-300">
+        @{new Date().getFullYear()}
+      </footer>
+    </>
+  )
+}
+
+export default Footer
