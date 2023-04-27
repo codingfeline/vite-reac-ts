52ab50aafeca54f782a13f46a2476615dec21c1f
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-27 21:13:57 +0100

    added imports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Home } from './home'
+export { default as Navbar } from './Navbar'
+export { default as Footer } from './Footer'
+export { default as About } from './about'
+export { default as AuthBox } from './auth'
+export { default as Restaurants } from './restaurants'
+export { default as OneRestaurant } from './OneRestaurant'
+export { default as AuthGit } from './auth_github'
