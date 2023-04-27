ff17e982cb665d668ec041c4400836cca6dd7661
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-04-27 21:15:37 +0100

    removed debugging log

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -5,6 +5,6 @@ const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
-console.log(supabase)
+// console.log(supabase)
 
 export default supabase
