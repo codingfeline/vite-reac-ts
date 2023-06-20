0f9a24c1f8a9f499e3cd2cf342968a69886b776b
Nazie Sharid <nazie.sh4rid@gmail.com>
2023-06-20 06:08:48 +0100

    get token

--- src/pages/signIn2.jsx
+++ src/pages/signIn2.jsx
@@ -1,38 +1,75 @@
 import axios from 'axios'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SignIn2 = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [error, setError] = useState('')
+  const [token, setToken] = useState('')
+  const [profile, setProfile] = useState(null)
+  const [userInfo, setUserInfo] = useState(null)
 
   const handleLogin = async e => {
     e.preventDefault()
+    setError('')
+    setToken('')
     try {
       const response = await axios.post('http://localhost:4400/login', { email, password })
-      console.log(response.data)
+      // console.log(response.data.data)
+      // console.log(response.data.data.session.access_token)
+      setToken(response.data.data.session.access_token)
+      setUserInfo(response.data.data.user)
     } catch (error) {
-      console.error(error.response.data.error)
+      console.error(error.response)
       setError(error.response.data.error)
     }
   }
 
+  const fetchProfile = async () => {
+    try {
+      const response = await axios.get('http://localhost:4400/profile', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      setProfile(response)
+    } catch (error) {
+      console.error(error.response.data.error)
+      setError(error.response)
+    }
+  }
+
+  useEffect(() => {
+    if (token) {
+      fetchProfile()
+    }
+  }, [token])
+
   return (
     <>
       <div>signIn2</div>
-      <form action="">
+      {profile ? (
         <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" onChange={e => setEmail(e.target.value)} />
-          {email}
+          <h2>Welcome, {email}!</h2>
+          <p>User ID: {}</p>
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
-        </div>
-        <button onClick={handleLogin}>SignIn</button>
-      </form>
+      ) : (
+        <form onSubmit={handleLogin}>
+          <div>
+            <label>Email:</label>
+            <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      )}
       {error && <p>error: {error}</p>}
+      {token && JSON.stringify(token)}
+      {profile ? JSON.stringify(profile) : error}
+      user: {userInfo && JSON.stringify(userInfo)}
     </>
   )
 }
